570aa46cbb91ddba8f2dfca3dc33ab44dc6ed0fb
NTF-15 <ntf-15@NTF-15.(none)>
2016-10-12 15:28:32 +0530

    Readme first

--- README.php
+++ README.php
@@ -4,7 +4,7 @@ Magento Ticket Support System
 Step1: After extracting the files.
 Step2: Paste all files in magento root.
 Step3: Check files/dir permission is 644/755
-step4: then import database file in your database wich is located in module DB folder
+step4: then import database file in your database which is located in module DB folder
 Step5: clear cache and indexes it will work proper
 
 ********Amit Patel*********
